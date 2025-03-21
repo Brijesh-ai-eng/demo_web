b384caad72408f01d75b0ac014e0921347384946
Brijesh-ai-eng <brijesh.225409@gmail.com>
2025-03-21 17:55:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st 
+import pickle
 
-
-name=st.text_input("Enter Your Name : ")
-fname=st.text_input("Enter Your Father's Name : ")
-address=st.text_area("Enter Your Address : ")
-classdata=st.selectbox("Enter Your Class : ",(1,2,3,4,5))
+st.subheader("Whether Prediction")
+pn=st.number_input("Enter precipitation")
+maxt=st.number_input("Enter maxt")
+mint=st.number_input("Enter mint")
+wind=st.number_input("Enter wind speed")
 button=st.button("Done")
 
 if button:
+    model=pickle.load(open("wp.pkl","rb"))
+    res=model.predict([[pn,maxt,mint,wind]])[0]
     st.markdown(f"""
-                Name : {name}
-                Father's Name : {fname}
-                Address : {address}
-                Class : {classdata}
-                """)
+    Whether Condition: {res}
+    """)
